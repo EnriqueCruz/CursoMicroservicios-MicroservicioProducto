edcbd97abed92032b550f66566f563df34f4516e
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-10-18 21:33:48 -0500

    Creación de clase de tipo Entidad, Producto

---
+++ src/main/java/com/springboot/app/productos/entity/Producto.java
@@ -0,0 +1,64 @@
+package com.springboot.app.productos.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="Productos")
+public class Producto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -590983016747559407L;
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	
+	
+	private String nombre;
+	private Double precio;
+	
+	@Column(name = "CREATE_AT")
+	@Temporal(TemporalType.DATE)
+	private Date fechaCreacion;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	
+	
+	
+}
