94795b47e78aa7e77595c1638ed1bb88e02e161e
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-10-18 21:42:23 -0500

    Se corrigio la interfaz que implementaba el Repositorio de métodosde conexión a base de datos

---
+++ src/main/java/com/springboot/app/productos/repository/ProductosDAO.java
@@ -0,0 +1,9 @@
+package com.springboot.app.productos.repository;
+
+import org.springframework.data.repository.Repository;
+
+import com.springboot.app.productos.entity.Producto;
+
+public class ProductosDAO implements Repository<Producto, Long> {
+
+}
