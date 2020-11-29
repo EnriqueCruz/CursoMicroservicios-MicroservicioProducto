0acb15e392fb51d230fd1fe374e06a7686b7097f
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-11-29 16:41:14 -0600

    Se agrega dependencia en el pom, configuraciones en el p y se adjunta el archivo import.sql que contiene los registros para probar el microservicio

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre,precio, CREATE_AT) VALUES('SMART TV Panasonic',7800,NOW());
+INSERT INTO productos (nombre,precio, CREATE_AT) VALUES('SMART TV Sony',5000,NOW());
+INSERT INTO productos (nombre,precio, CREATE_AT) VALUES('Bicileta Fija Reebok',5000,NOW());
